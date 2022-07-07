f1d0d11007a3b6ee2d863a462061bcfe197b7bfb
theProgrammingBox <justinboxgea@gmail.com>
2022-07-06 23:30:55 -0700

    done, might add iterator pointer operators

--- Vector2/Source.cpp
+++ Vector2/Source.cpp
@@ -1,126 +1,24 @@
-//#include "Vector2_.h"
-#include <iostream>
-
-using OBJECT = std::pair<int, void*>;
-using ITERATOR = OBJECT*;
-using ITERATOR_POINTER = ITERATOR*;
-
-size_t currentSize;
-size_t maxSize;
-ITERATOR arr = nullptr;
-
-void new_()
-{
-	currentSize = 0;
-	maxSize = 1;
-	arr = new OBJECT[1];
-}
-
-void expand()
-{
-	if (currentSize == maxSize)
-	{
-		maxSize = maxSize << 1;
-
-		ITERATOR newArr = new OBJECT[maxSize];
-		memcpy(newArr, arr, sizeof(OBJECT) * currentSize);
-		delete[] arr;
-		arr = newArr;
-
-		for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
-		{
-			*(ITERATOR_POINTER)i->second = i;
-		}
-	}
-}
-
-void push_back(const int data)
-{
-	expand();
-	arr[currentSize].first = data;
-	arr[currentSize].second = new ITERATOR(arr + currentSize);
-	currentSize++;
-}
-
-void pop()
-{
-	if (currentSize != 0)
-	{
-		delete arr[--currentSize].second;
-	}
-}
-
-void insert(const ITERATOR_POINTER iteratorPointer, const int data)
-{
-	expand();
-	memmove(
-		*iteratorPointer + 1,
-		*iteratorPointer,
-		sizeof(OBJECT) * (arr + currentSize - *iteratorPointer)
-	);
-	(*iteratorPointer)->first = data;
-	(*iteratorPointer)->second = new ITERATOR(*iteratorPointer);
-	for (ITERATOR i = arr + currentSize++; i > *iteratorPointer; i--)
-	{
-		*(ITERATOR_POINTER)i->second = i;
-	}
-}
-
-void erase(const ITERATOR_POINTER iteratorPointer)
-{
-	memmove(
-		*iteratorPointer,
-		*iteratorPointer + 1,
-		sizeof(OBJECT) * (arr + currentSize - *iteratorPointer)
-	);
-	for (ITERATOR i = arr + --currentSize - 1; i >= *iteratorPointer; i--)
-	{
-		*(ITERATOR_POINTER)i->second = i;
-	}
-	delete iteratorPointer;
-}
-
-void print()
-{
-	int j = currentSize;
-	for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
-	{
-		std::cout << --j << ": "
-			<< i->first << '\t'
-			<< (*(ITERATOR_POINTER)i->second)->first
-			<< std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void delete_()
-{
-	for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
-	{
-		delete i->second;
-	}
-	delete[] arr;
-}
+#include "Vector2_.h"
 
 int main()
 {
-	new_();
-	push_back(101);
-	push_back(321);
-	push_back(345);
-	print();
-	pop();
-	print();
-	push_back(456);
-	push_back(645);
-	push_back(978);
-	print();
-	insert((ITERATOR_POINTER)arr[0].second, 789);
-	insert((ITERATOR_POINTER)arr[4].second, 789);
-	print();
-	erase((ITERATOR_POINTER)arr[4].second);
-	print();
-	delete_();
+	Vector2<int> v;
+	v.push_back(101);
+	v.push_back(321);
+	v.push_back(345);
+	v.pop();
+	v.push_back(456);
+	v.push_back(645);
+	v.push_back(978);
+	v.insert(v.begin(), 789);
+	v.insert(v.end(), 789);
+	v.erase(v.begin());
+	v.erase(v.end());
+
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		std::cout << v[i] << std::endl;
+	}
 
 	return 0;
 }
--- Vector2/Vector2_.h
+++ Vector2/Vector2_.h
@@ -1,112 +1,111 @@
-//#pragma once
-//#include <iostream>
-//
-//template <typename OBJECT_TYPE> class Vector2
-//{
-//	using OBJECT = std::pair<OBJECT_TYPE, void *>;
-//	using PERM_ITERATOR = OBJECT*;
-//	using TEMP_ITERATOR = OBJECT_TYPE*;
-//private:
-//	size_t currentSize;
-//	size_t maxSize;
-//	OBJECT* arr = nullptr;
-//
-//	void expand()
-//	{
-//		if (currentSize == maxSize)
-//		{
-//			maxSize = maxSize << 1;
-//
-//			OBJECT* newArr = new OBJECT[maxSize];
-//			memcpy(newArr, arr, sizeof(OBJECT) * currentSize);
-//			delete[] arr;
-//			arr = newArr;
-//
-//			for (TEMP_ITERATOR i = arr + currentSize; i >= arr; --i)
-//			{
-//				*(i.second) = i;
-//			}
-//		}
-//	}
-//
-//public:
-//	Vector2()
-//	{
-//		currentSize = 0;
-//		maxSize = 1;
-//		arr = new OBJECT[1];
-//	}
-//
-//	~Vector2()
-//	{
-//		for (TEMP_ITERATOR i = arr + currentSize; i >= arr; --i)
-//		{
-//			delete i.second;
-//		}
-//		delete[] arr;
-//	}
-//
-//	void push_back(const OBJECT_TYPE data)
-//	{
-//		expand();
-//		arr[currentSize].first = data;
-//		arr[currentSize++].second = new TEMP_ITERATOR(arr + currentSize);
-//	}
-//
-//	void pop()
-//	{
-//		if (currentSize != 0)
-//		{
-//			delete (arr + --currentSize).second;
-//		}
-//	}
-//
-//	void insert(PERM_ITERATOR iterator, const OBJECT_TYPE data)
-//	{
-//		expand();
-//		memmove(
-//			*iterator + 1,
-//			*iterator,
-//			sizeof(OBJECT) * (arr + currentSize++ - *iterator)
-//		);
-//		for (TEMP_ITERATOR i = arr + currentSize; i != *iterator; --i)
-//		{
-//			*(i.second) = i;
-//		}
-//		*iterator.first = data;
-//		*iterator.second = new TEMP_ITERATOR(*iterator);
-//	}
-//
-//	void erase(PERM_ITERATOR iterator)
-//	{
-//		delete iterator.second;
-//		if (iterator != arr + --currentSize)
-//		{
-//			memmove(
-//				*iterator,
-//				*iterator + 1,
-//				sizeof(OBJECT) * (arr + currentSize - *iterator)
-//			);
-//			for (TEMP_ITERATOR i = arr + currentSize; i >= *iterator; --i)
-//			{
-//				*(i.second) = i;
-//			}
-//		}
-//	}
-//
-//	void clear()
-//	{
-//		for (TEMP_ITERATOR i = arr + currentSize; i >= arr; --i)
-//		{
-//			delete i.second;
-//		}
-//		currentSize = 0;
-//	}
-//
-//	size_t size() const { return currentSize; }
-//	OBJECT_TYPE operator[](const size_t index) const { return arr[index].first; }
-//	OBJECT_TYPE& operator[](const size_t index) { return arr[index].first; }
-//
-//	PERM_ITERATOR begin() const { return arr.second; }
-//	PERM_ITERATOR end() const { return (arr + currentSize).second; }
-//};
+#pragma once
+#include <iostream>
+
+template <typename OBJECT_TYPE> class Vector2
+{
+	using OBJECT = std::pair<OBJECT_TYPE, void*>;
+	using ITERATOR = OBJECT*;
+	using ITERATOR_POINTER = ITERATOR*;
+
+public:
+	Vector2()
+	{
+		currentSize = 0;
+		maxSize = 1;
+		arr = new OBJECT[1];
+	}
+
+	~Vector2()
+	{
+		for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
+		{
+			delete i->second;
+		}
+		delete[] arr;
+	}
+
+	void push_back(const OBJECT_TYPE data)
+	{
+		expand();
+		arr[currentSize].first = data;
+		arr[currentSize].second = new ITERATOR(arr + currentSize);
+		currentSize++;
+	}
+
+	void pop()
+	{
+		if (currentSize != 0)
+		{
+			delete arr[--currentSize].second;
+		}
+	}
+
+	void insert(const ITERATOR_POINTER iteratorPointer, const OBJECT_TYPE data)
+	{
+		expand();
+		memmove(
+			*iteratorPointer + 1,
+			*iteratorPointer,
+			sizeof(OBJECT) * (arr + currentSize - *iteratorPointer)
+		);
+		(*iteratorPointer)->first = data;
+		(*iteratorPointer)->second = new ITERATOR(*iteratorPointer);
+		for (ITERATOR i = arr + currentSize++; i > *iteratorPointer; i--)
+		{
+			*(ITERATOR_POINTER)i->second = i;
+		}
+	}
+
+	void erase(const ITERATOR_POINTER iteratorPointer)
+	{
+		memmove(
+			*iteratorPointer,
+			*iteratorPointer + 1,
+			sizeof(OBJECT) * (arr + currentSize - *iteratorPointer)
+		);
+		for (ITERATOR i = arr + --currentSize - 1; i >= *iteratorPointer; i--)
+		{
+			*(ITERATOR_POINTER)i->second = i;
+		}
+		delete iteratorPointer;
+	}
+
+	void clear()
+	{
+		for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
+		{
+			delete i->second;
+		}
+		currentSize = 0;
+	}
+
+	size_t size() const { return currentSize; }
+	OBJECT_TYPE operator[](const size_t index) const { return arr[index].first; }
+	OBJECT_TYPE& operator[](const size_t index) { return arr[index].first; }
+
+	ITERATOR_POINTER begin() const { return (ITERATOR_POINTER)arr->second; }
+	ITERATOR_POINTER end() const { return (ITERATOR_POINTER)(arr + currentSize - 1)->second; }
+
+private:
+	size_t currentSize;
+	size_t maxSize;
+	ITERATOR arr = nullptr;
+
+	void expand()
+	{
+		if (currentSize == maxSize)
+		{
+			maxSize = maxSize << 1;
+
+			ITERATOR newArr = new OBJECT[maxSize];
+			memcpy(newArr, arr, sizeof(OBJECT) * currentSize);
+			delete[] arr;
+			arr = newArr;
+
+			for (ITERATOR i = arr + currentSize - 1; i >= arr; i--)
+			{
+				*(ITERATOR_POINTER)i->second = i;
+			}
+		}
+	}
+};
